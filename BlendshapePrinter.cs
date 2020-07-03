f61876fcc1c770ebc5a9e0f3b65dd2e29c5c6919
ikds temp <>
2020-07-04 02:15:04 +0900

    ふぐ顔をするとGUIに5秒キープ！と表示

---
+++ BlendshapePrinter.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class BlendshapePrinter : MonoBehaviour
+{
+
+	bool shapeEnabled = false;
+	Dictionary<string, float> currentBlendShapes;
+
+	// Use this for initialization
+	void Start()
+	{
+		UnityARSessionNativeInterface.ARFaceAnchorAddedEvent += FaceAdded;
+		UnityARSessionNativeInterface.ARFaceAnchorUpdatedEvent += FaceUpdated;
+		UnityARSessionNativeInterface.ARFaceAnchorRemovedEvent += FaceRemoved;
+
+	}
+
+	void OnGUI()
+	{
+		if (shapeEnabled)
+		{
+
+			string blendshapes = "";
+			string shapeNames = "";
+			string valueNames = "";
+			/*foreach (KeyValuePair<string, float> kvp in currentBlendShapes)
+			{
+				blendshapes += " [";
+				blendshapes += kvp.Key.ToString();
+				blendshapes += ":";
+				blendshapes += kvp.Value.ToString();
+				blendshapes += "]\n";
+				shapeNames += "\"";
+				shapeNames += kvp.Key.ToString();
+				shapeNames += "\",\n";
+				valueNames += kvp.Value.ToString();
+				valueNames += "\n";
+			}*/
+
+
+
+			if (currentBlendShapes["cheekPuff"] >= 0.2)
+			{
+				blendshapes += "5秒キープ！";
+			}
+
+			//GUI.skin.box.fontSize = 22;
+			GUI.skin.box.fontSize = 50;
+			GUILayout.BeginHorizontal(GUILayout.ExpandHeight(true));
+			GUILayout.Box(blendshapes);
+			//GUILayout.Box(keep);
+			GUILayout.EndHorizontal();
+
+
+			Debug.Log(shapeNames);
+			Debug.Log(valueNames);
+
+		}
+
+
+	}
+
+	void FaceAdded(ARFaceAnchor anchorData)
+	{
+		shapeEnabled = true;
+		currentBlendShapes = anchorData.blendShapes;
+	}
+
+	void FaceUpdated(ARFaceAnchor anchorData)
+	{
+		currentBlendShapes = anchorData.blendShapes;
+	}
+
+	void FaceRemoved(ARFaceAnchor anchorData)
+	{
+		shapeEnabled = false;
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
